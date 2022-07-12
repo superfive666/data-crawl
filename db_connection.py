3ddbe9867ad9989c5f386ad6ea6f9df9a6361092
Wu chao <chao.wu@bitdeer.com>
2022-07-12 16:35:17 +0800

    Update the postgresql connection driver to use version 3 instead

--- db_connection.py
+++ db_connection.py
@@ -1,8 +1,8 @@
-import psycopg2
+import psycopg
 
 def get_connection():
     # set search_path = data
-    conn = psycopg2.connect(
+    conn = psycopg.connect(
         host="ok-db-1.cmqzhxowhqdn.ap-southeast-1.rds.amazonaws.com",
         database="datacrawl",
         user="app_user",
